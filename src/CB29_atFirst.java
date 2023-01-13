536694dc0a36755dc5eb72a7a0a41cc64fd45c5a
halil <halilakgun0@gmail.com>
2023-01-13 14:45:10 +0300

    CB30_lastTwo

--- src/CB29_atFirst.java
+++ src/CB29_atFirst.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class CB29_atFirst {
+/*
+Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2,
+use '@' for the missing chars.
+atFirst("hello") → "he"
+atFirst("hi") → "hi"
+atFirst("h") → "h@"
+ */
+public class CB29_atFirst {
+    public static void main(String[] args) {
+        System.out.println(atFirst("h"));
+    }
+
+    public static String atFirst(String str) {
+        if (str.length() == 1) {
+            return str + "@";
+        } else if (str.length() > 1) {
+            return str.substring(0, 2);
+        } else return "@@";
+    }
 }
--- src/CB30_lastTwo.java
+++ src/CB30_lastTwo.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class CB30_lastTwo {
+/*
+Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+lastTwo("coding") → "codign"
+lastTwo("cat") → "cta"
+lastTwo("ab") → "ba"
+ */
+public class CB30_lastTwo {
+    public static void main(String[] args) {
+        System.out.println(lastTwo("aq"));
+    }
+
+    public static String lastTwo(String str) {
+        StringBuilder a = new StringBuilder(str);
+        String s1 = "";
+        String s2 = "";
+        if (str.length() > 1) {
+            s1 = str.substring(str.length() - 1);
+            s2 = str.substring(str.length() - 2, str.length() - 1);
+            a.delete(a.length() - 2, a.length());
+            return a.toString() + s1 + s2;
+        }
+        return a.toString() + s1 + s2;
+    }
 }
