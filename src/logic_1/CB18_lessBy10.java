980f40883cdea88a70badd8e0129361b0b798719
halil <halilakgun0@gmail.com>
2023-03-02 22:16:52 +0300

    CB18_lessBy10

---
+++ src/logic_1/CB18_lessBy10.java
@@ -0,0 +1,20 @@
+package logic_1;
+
+/*
+Given three ints, a b c, return true if one of them is 10 or more less than one of the others.
+
+lessBy10(1, 7, 11) → true
+lessBy10(1, 7, 10) → false
+lessBy10(11, 1, 7) → true
+ */
+public class CB18_lessBy10 {
+    public static void main(String[] args) {
+        System.out.println(lessBy10(1, 7, 10));
+    }
+
+    public static boolean lessBy10(int a, int b, int c) {
+        if (Math.abs(a - b) > 9) return true;
+        if (Math.abs(a - c) > 9) return true;
+        return Math.abs(b - c) > 9;
+    }
+}
