8a47935831282c1c02f0305359088a3e8deeea4e
halil <halilakgun0@gmail.com>
2023-03-09 13:35:05 +0300

    CB28_withoutDoubles

---
+++ src/logic_1/CB27_inOrderEqual.java
@@ -0,0 +1,19 @@
+package logic_1;
+
+/*
+Given three ints, a b c, return true if they are in strict increasing order, such as 2 5 11, or 5 6 7, but not 6 5 7 or 5 5 7.
+However, with the exception that if "equalOk" is true, equality is allowed, such as 5 5 7 or 5 5 5.
+
+inOrderEqual(2, 5, 11, false) → true
+inOrderEqual(5, 7, 6, false) → false
+inOrderEqual(5, 5, 7, true) → true
+ */
+public class CB27_inOrderEqual {
+    public static void main(String[] args) {
+        System.out.println(inOrderEqual(2, 5, 11, false));
+    }
+
+    public static boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+        return (equalOk && c >= b && b >= a) || (c > b && b > a);
+    }
+}
---
+++ src/logic_1/CB28_withoutDoubles.java
@@ -0,0 +1,23 @@
+package logic_1;
+
+/*
+Return the sum of two 6-sided dice rolls, each in the range 1..6. However, if noDoubles is true,
+if the two dice show the same value, increment one die to the next value, wrapping around to 1 if its value was 6.
+
+withoutDoubles(2, 3, true) → 5
+withoutDoubles(3, 3, true) → 7
+withoutDoubles(3, 3, false) → 6
+ */
+public class CB28_withoutDoubles {
+    public static void main(String[] args) {
+        System.out.println(withoutDoubles(2, 3, true));
+    }
+
+    public static int withoutDoubles(int die1, int die2, boolean noDoubles) {
+        if (noDoubles) {
+            if (die1 == die2 && die1 == 6) return 7;
+            if (die1 == die2) return die1 + die2 + 1;
+        }
+        return die1 + die2;
+    }
+}
