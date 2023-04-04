9008d7c6f3d593086418f0f605a4daa703d0ddfa
halil-akgun <halilakgun0@gmail.com>
2023-04-04 12:14:53 +0300

    CB06_sameStarChar

---
+++ src/String_2/CB06_sameStarChar.java
@@ -0,0 +1,22 @@
+package String_2;
+
+/*
+Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
+
+sameStarChar("xy*yzz") → true
+sameStarChar("xy*zzz") → false
+sameStarChar("*xa*az") → true
+ */
+public class CB06_sameStarChar {
+    public static void main(String[] args) {
+        System.out.println(sameStarChar("*xa*az"));
+    }
+
+    public static boolean sameStarChar(String str) {
+        for(int i = 1; i < str.length() - 1; i++) {
+            if(str.charAt(i) == '*' && str.charAt(i - 1) != str.charAt(i + 1))
+                return false;
+        }
+        return true;
+    }
+}
