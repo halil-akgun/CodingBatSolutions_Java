3dd4cda39cc617ea18737869eb37d86f1f8edebb
halil <halilakgun0@gmail.com>
2023-04-24 21:03:24 +0300

    CB06_countTriple

---
+++ src/String_3/CB06_countTriple.java
@@ -0,0 +1,17 @@
+package String_3;
+/*
+We'll say that a "triple" in a string is a char appearing three times in a row. Return the number of
+triples in the given string. The triples may overlap.
+
+countTriple("abcXXXabc") → 1
+countTriple("xxxabyyyycd") → 3
+countTriple("a") → 0
+ */
+public class CB06_countTriple {
+    public static void main(String[] args) {
+        System.out.println(countTriple("abc123xyz"));
+    }
+    public static int countTriple(String str) {
+
+    }
+}
