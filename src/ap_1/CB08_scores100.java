fdfdea6d83e1034720c305e4cc92aa48e1e2baac
halil <halilakgun0@gmail.com>
2023-07-05 17:35:00 +0300

    added way 2

--- src/ap_1/CB08_scores100.java
+++ src/ap_1/CB08_scores100.java
@@ -1,5 +1,7 @@
 package ap_1;
 
+import java.util.Arrays;
+
 /*
 Given an array of scores, return true if there are scores of 100 next to each other
 in the array. The array length will be at least 2.
@@ -15,9 +17,15 @@ public class CB08_scores100 {
     }
 
     public static boolean scores100(int[] scores) {
-        for (int i = 1; i < scores.length; i++) {
-            if (scores[i - 1] == 100 && scores[i] == 100) return true;
-        }
-        return false;
+//        way 1
+//        for (int i = 1; i < scores.length; i++) {
+//            if (scores[i - 1] == 100 && scores[i] == 100) return true;
+//        }
+//        return false;
+
+
+//        way 2
+        String str = Arrays.toString(scores).replaceAll("[^0-9]", "*");
+        return str.contains("*100**100*");
     }
 }
